257d5be2c28bd109a73c13a9225b93b33f2bc37c
Smokey <9442271+smokey019@users.noreply.github.com>
2024-09-02 08:25:28 -0600

    initial upload

---
+++ BetterSoundOptions.lua
@@ -0,0 +1,188 @@
+local BetterSoundOptions = LibStub("AceAddon-3.0"):NewAddon("BetterSoundOptions")
+
+local defaultAvoidedMusicIDs = {}
+local alreadyPlayedMusicIDs = {}
+
+BetterSoundOptionsDB = BetterSoundOptionsDB or {
+	settingsKeys = {}, avoidedMusicIDs = defaultAvoidedMusicIDs
+}
+
+-- Create a frame
+local f = CreateFrame("Frame")
+local MusicCD = time() - 121
+local currentMusicID
+
+-- Register events
+f:RegisterEvent("PET_BATTLE_CLOSE")
+f:RegisterEvent("CINEMATIC_STOP")
+f:RegisterEvent("SOUND_DEVICE_UPDATE")
+f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+f:RegisterEvent("ZONE_CHANGED")
+f:RegisterEvent("PLAYER_LOGIN")
+f:RegisterEvent("TALKINGHEAD_REQUESTED")
+f:SetScript("OnEvent", function(self, event, ...)
+	if event == "PET_BATTLE_CLOSE" then
+		C_Timer.After(0.1, function() BetterSoundOptions:UpdateWorld(BetterSoundOptions:GetProfileType()) end)
+	elseif event == "CINEMATIC_STOP" then
+		C_Timer.After(0.1, function() BetterSoundOptions:UpdateWorld(BetterSoundOptions:GetProfileType()) end)
+	elseif event == "SOUND_DEVICE_UPDATE" then
+		C_Timer.After(0.1, function() BetterSoundOptions:UpdateWorld(BetterSoundOptions:GetProfileType()) end)
+	elseif event == "ZONE_CHANGED_NEW_AREA" then
+		C_Timer.After(0.1, function() BetterSoundOptions:UpdateWorld(BetterSoundOptions:GetProfileType()) end)
+	elseif event == "ZONE_CHANGED" then
+		C_Timer.After(0.1, function() BetterSoundOptions:UpdateWorld(BetterSoundOptions:GetProfileType()) end)
+	elseif event == "PLAYER_LOGIN" then
+		BetterSoundOptionsDB.PreviousDialogueSetting = GetCVar("Sound_EnableDialog")
+		C_Timer.After(0.1, function() BetterSoundOptions:UpdateWorld(BetterSoundOptions:GetProfileType()) end)
+		if not BetterSoundOptionsDB.avoidedMusicIDs then
+			BetterSoundOptionsDB.avoidedMusicIDs = {}
+		end
+	elseif event == "TALKINGHEAD_REQUESTED" then
+		local inInstance, instanceType = IsInInstance()
+		if inInstance and instanceType == "party" and BetterSoundOptionsDB.settingsKeys.dungeonDisableTH or BetterSoundOptionsDB.settingsKeys.alwaysDisableTH then
+			BetterSoundOptions:CloseTalkingHead()
+		end
+	end
+end)
+
+local function has_value(tab, val)
+	for index, value in ipairs(tab) do
+		if value == val then
+			return true
+		end
+	end
+
+	return false
+end
+
+SLASH_AVOIDMUSIC1 = '/avoid'
+function SlashCmdList.AVOIDMUSIC(msg, editBox)
+	if not has_value(BetterSoundOptionsDB.avoidedMusicIDs, msg) then
+		table.insert(BetterSoundOptionsDB.avoidedMusicIDs, msg)
+		print('Will avoid music id:', msg)
+	else
+		print('Already avoided that Music ID.')
+	end
+end
+
+SLASH_CLEARAVOIDMUSIC1 = '/clearavoid'
+function SlashCmdList.CLEARAVOIDMUSIC(msg, editBox)
+	if msg == 'yesdoitpleaseuwu' then
+		BetterSoundOptionsDB.avoidedMusicIDs = defaultAvoidedMusicIDs
+		print('Cleared avoided Music ID lists')
+	end
+end
+
+function BetterSoundOptions:CloseTalkingHead(self, evt, text, source, xtra)
+	if BetterSoundOptionsDB.settingsKeys.debugText then
+		local displayInfo, cameraID, vo, duration, lineNumber, numLines, name, text, isNewTalkingHead, textureKit =
+			C_TalkingHead.GetCurrentLineInfo();
+		BetterSoundOptions:DebugText(string.format("Closing chat %s: %s", name, text));
+		BetterSoundOptions:DebugText(string.format("Debug data: %s %s %s %s %s %s", displayInfo, cameraID, vo, duration,
+			lineNumber, numLines, isNewTalkingHead, textureKit));
+	end
+	TalkingHeadFrame:PlayCurrent();
+	TalkingHeadFrame:CloseImmediately();
+end
+
+function BetterSoundOptions:ToggleMainFrame()
+	if not f:IsShown() then
+		f:Show()
+	else
+		f:Hide()
+	end
+end
+
+function BetterSoundOptions:PlayRandomMusic(music)
+	BetterSoundOptions:DebugText('attempting to play music:', music)
+
+	if not BetterSoundOptionsDB.avoidedMusicIDs[music] and not alreadyPlayedMusicIDs[music] then
+		PlayMusic(music)
+
+		MusicCD = time()
+		currentMusicID = music
+		table.insert(alreadyPlayedMusicIDs, music)
+	else
+		BetterSoundOptions:DebugText('avoided music detected, attempting to play something else:', music)
+
+		music = BetterSoundOptions:random_elem(BetterSoundOptions.listfile_music)
+
+		BetterSoundOptions:PlayRandomMusic(music)
+	end
+end
+
+function BetterSoundOptions:SetMusic()
+	print('Playing new random Music.  Old music ID to report in case it was an annoying sound: ', currentMusicID)
+
+	local music = BetterSoundOptions:random_elem(BetterSoundOptions.listfile_music)
+
+	BetterSoundOptions:PlayRandomMusic(music)
+end
+
+function BetterSoundOptions:random_elem(tb)
+	local keys = {}
+	for k, v in pairs(tb) do table.insert(keys, k) end
+	return tb[keys[math.random(#keys)]]
+end
+
+function BetterSoundOptions:randFrom(t)
+	local choice = "F"
+	local n = 0
+	for i, o in pairs(t) do
+		n = n + 1
+		if math.random() < (1 / n) then
+			choice = o
+		end
+	end
+	return choice
+end
+
+function BetterSoundOptions:UpdateWorld(pt, subzone)
+	if InCinematic() then
+		return
+	end
+
+	local inInstance, instanceType = IsInInstance()
+
+	if inInstance and BetterSoundOptionsDB.settingsKeys.disableDialogue and instanceType == "party" then
+		local mapID = C_Map.GetBestMapForUnit("player");
+		local parentMapID = C_Map.GetMapInfo(C_Map.GetMapInfo(mapID).parentMapID)
+		if not BetterSoundOptionsDB.settingsKeys.instancesVisited[parentMapID] then
+			BetterSoundOptionsDB.settingsKeys.instancesVisited.insert(parentMapID)
+		else
+			BetterSoundOptionsDB.PreviousDialogueSetting = GetCVar("Sound_EnableDialog")
+			SetCVar("Sound_EnableDialog", 0)
+			BetterSoundOptions:DebugText('previously entered dungeon detected. disabling dialogue.')
+		end
+	elseif not IsInInstance() and BetterSoundOptionsDB.settingsKeys.disableDialogue then
+		SetCVar("Sound_EnableDialog", BetterSoundOptionsDB.PreviousDialogueSetting)
+	end
+
+	if (time() - MusicCD) > 30 and BetterSoundOptionsDB.settingsKeys.randomizeMusic then
+		local music = BetterSoundOptions:randFrom(BetterSoundOptions.listfile_music)
+		BetterSoundOptions:PlayRandomMusic(music)
+	end
+end
+
+function BetterSoundOptions:GetInstanceType()
+	if IsResting() then
+		return "rest"
+	end
+	local _, v = GetInstanceInfo()
+	return v;
+end
+
+function BetterSoundOptions:GetProfileType()
+	local profile = self.db.profile
+	local instance = self:GetInstanceType()
+	if instance ~= "none" and profile["enable_" .. instance] then
+		return instance;
+	end
+	return "none"
+end
+
+function BetterSoundOptions:DebugText(text, ...)
+	if BetterSoundOptionsDB.settingsKeys.debugText then
+		print(text, ...)
+	end
+end
---
+++ Settings.lua
@@ -0,0 +1,147 @@
+local addon = LibStub("AceAddon-3.0"):GetAddon("BetterSoundOptions")
+BetterSoundOptionsMinimapButton = LibStub("LibDBIcon-1.0", true)
+
+local checkboxes = 0
+
+local settings = {
+	{
+		settingText = "Randomize Music",
+		settingKey = "randomizeMusic",
+		settingTooltip = "While enabled, music will be ranzomized. Avoid music by using /avoid musicID",
+		checked = true,
+	},
+	{
+		settingText = "Disable Dialogue In Instances After First Visit",
+		settingKey = "disableDialogue",
+		settingTooltip =
+		"While enabled, dialogue will be disabled after first visit of an instance.  This will take effect after initial installation.",
+		checked = false,
+	},
+	{
+		settingText = "Always close Talking Head dialogue.",
+		settingKey = "alwaysDisableTH",
+		settingTooltip =
+		"While enabled, this addon will always close talking head dialogues even outside of dungeons.",
+		checked = false,
+	},
+	{
+		settingText = "Only close Talking Head dialogue in dungeons.",
+		settingKey = "dungeonDisableTH",
+		settingTooltip =
+		"While enabled, this addon will close talking head dialogues while inside of dungeons.",
+		checked = false,
+	},
+	{
+		settingText = "Enable Debug Text",
+		settingKey = "debugText",
+		settingTooltip = "While enabled, debug text will display in chat to help diagnose errors.",
+		checked = true,
+	},
+}
+
+local settingsFrame = CreateFrame("Frame", "BetterSoundOptionsSettingsFrame", UIParent, "BasicFrameTemplateWithInset")
+settingsFrame:SetSize(400, 300)
+settingsFrame:SetPoint("CENTER")
+settingsFrame.TitleBg:SetHeight(30)
+settingsFrame.title = settingsFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+settingsFrame.title:SetPoint("TOP", settingsFrame.TitleBg, "TOP", 0, -3)
+settingsFrame.title:SetText("Better Sound Options by Smokey")
+settingsFrame:Hide()
+settingsFrame:EnableMouse(true)
+settingsFrame:SetMovable(true)
+settingsFrame:RegisterForDrag("LeftButton")
+settingsFrame:SetScript("OnDragStart", function(self)
+	self:StartMoving()
+end)
+
+settingsFrame:SetScript("OnDragStop", function(self)
+	self:StopMovingOrSizing()
+end)
+
+local function CreateCheckbox(checkboxText, key, checkboxTooltip, checked)
+	local checkbox = CreateFrame("CheckButton", "BetterSoundOptionsCheckboxID" .. checkboxes, settingsFrame,
+		"UICheckButtonTemplate")
+	checkbox.Text:SetText(checkboxText)
+	checkbox:SetPoint("TOPLEFT", settingsFrame, "TOPLEFT", 10, -30 + (checkboxes * -30))
+
+	if BetterSoundOptionsDB.settingsKeys[key] == nil then
+		BetterSoundOptionsDB.settingsKeys[key] = checked
+	end
+
+	checkbox:SetChecked(BetterSoundOptionsDB.settingsKeys[key])
+
+	checkbox:SetScript("OnEnter", function(self)
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+		GameTooltip:SetText(checkboxTooltip, nil, nil, nil, nil, true)
+	end)
+
+	checkbox:SetScript("OnLeave", function(self)
+		GameTooltip:Hide()
+	end)
+
+	checkbox:SetScript("OnClick", function(self)
+		BetterSoundOptionsDB.settingsKeys[key] = self:GetChecked()
+	end)
+
+	checkboxes = checkboxes + 1
+
+	return checkbox
+end
+
+local eventListenerFrame = CreateFrame("Frame", "BetterSoundOptionsSettingsEventListenerFrame", UIParent)
+
+eventListenerFrame:RegisterEvent("PLAYER_LOGIN")
+
+eventListenerFrame:SetScript("OnEvent", function(self, event)
+	if event == "PLAYER_LOGIN" then
+		if not BetterSoundOptionsDB.settingsKeys then
+			BetterSoundOptionsDB.settingsKeys = {}
+		end
+
+		for _, setting in pairs(settings) do
+			CreateCheckbox(setting.settingText, setting.settingKey, setting.settingTooltip, setting.checked)
+		end
+	end
+end)
+
+local miniButton = LibStub("LibDataBroker-1.1"):NewDataObject("BetterSoundOptions", {
+	type = "data source",
+	text = "BetterSoundOptions",
+	icon = "Interface/Icons/TRADE_ARCHAEOLOGY_DELICATEMUSICBOX",
+	OnClick = function(self, btn)
+		if btn == "LeftButton" and BetterSoundOptionsDB.settingsKeys.randomizeMusic then
+			addon:SetMusic()
+		elseif btn == "RightButton" then
+			if settingsFrame:IsShown() then
+				settingsFrame:Hide()
+			else
+				settingsFrame:Show()
+			end
+		end
+	end,
+
+	OnTooltipShow = function(tooltip)
+		if not tooltip or not tooltip.AddLine then
+			return
+		end
+
+		tooltip:AddLine(
+			"BetterSoundOptions\n\nLeft-click: Change random music (if enabled). \nRight-click: Open Better Sound Options Settings",
+			nil,
+			nil, nil, nil)
+	end,
+})
+
+function addon:OnInitialize()
+	self.db = LibStub("AceDB-3.0"):New("BetterSoundOptionsMinimapPOS", {
+		profile = {
+			minimap = {
+				hide = false,
+			},
+		},
+	})
+
+	BetterSoundOptionsMinimapButton:Register("BetterSoundOptions", miniButton, self.db.profile.minimap)
+end
+
+BetterSoundOptionsMinimapButton:Show("BetterSoundOptions")
